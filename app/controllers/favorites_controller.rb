67ed6bf7d4d412801f53835531f4a4f69634ae3e
github username <github email address>
2020-02-14 09:27:58 -0500

    Added reject to favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,7 @@
 class FavoritesController < ApplicationController
     
     def create
+        return head(:forbidden) unless logged_in?
         Favorite.create(user_id: session[:user_id], character_id: params[:character_id])
         redirect_to request.referer
     end
